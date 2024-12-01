ba8fbdf152efc973f2c327e024b413ce3ff0ee07
Ronald-8-ball <ronaldokuk462@gmail.com>
2024-12-01 17:55:45 +0100

    Math sucks

--- REO.py
+++ REO.py
@@ -1,5 +1,20 @@
-class math:
-    num1 = 2
-    num2 = 4
-    print("2 + 4 =",num1 + num2)
-obj = math()
+class NumberOperations:
+    def __init__(self, num1, num2, num3):
+        self.num1 = num1
+        self.num2 = num2
+        self.num3 = num3
+
+    def add_numbers(self):
+        result = self.num1 + self.num2 + self.num3
+        print(f"The sum of {self.num1}, {self.num2}, and {self.num3} is: {result}")
+
+# Creating objects
+obj1 = NumberOperations(10, 20, 30)
+obj2 = NumberOperations(5, 15, 25)
+
+# Implementing the method
+obj1.add_numbers()
+obj2.add_numbers()
+
+
+
